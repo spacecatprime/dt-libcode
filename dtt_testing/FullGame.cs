7f3de1718a8148d8f110bea7c9a48bb16b22cf9a
ajackson <aajtiny@gmail.com>
2015-04-08 22:33:33 -0500

    added a new test framework

---
+++ dtt_testing/FullGame.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using dtlibcode;
+
+namespace dtt_testing
+{
+	[TestFixture()]
+	public class FullGameTests
+	{
+		[Test()]
+		public void GameSetup()
+		{
+		}
+
+		[Test()]
+		public void SetupScenarioAndPlayOneRound()
+		{
+		}
+
+		[Test()]
+		public void SetupScenarioAndPlayFullGame()
+		{
+		}
+	}
+}
+
